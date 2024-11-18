a15a380dfb3ce02f155a4d028bb7d742b4e0b005
danrfletcher <danrfletcher@outlook.com>
2024-11-18 18:30:39 +0000

    initial solution to isUnique

---
+++ Chapter 1 - Arrays & Strings/1.1 isUnique.js
@@ -0,0 +1,29 @@
+/**
+ * --- PROBLEM DESCRIPTION ---
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+ */
+/*
+
+/**
+ * --- INITIAL SOLUTION ---
+ * Iterate through string, create bucket for each new character, add 1 to the count each time it is encountered.
+ */
+
+const characters = Map();
+
+const isUnique = (string) => {
+	for (char in string) {
+		characters.has(char) ? 
+		return false : 
+		characters.set(char.toString(), {
+			count: 1	
+		})
+	};
+	return true;
+}
+
+/**
+ * --- EXPLANATION ---
+ * The above solution has runtime O(N), which is the best possible outcome given the requirement that the algorithm check the string for duplicate characters.
+ */
+
