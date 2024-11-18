05eb1008bff08c7b3a161816f2f801f8f5122a01
danrfletcher <danrfletcher@outlook.com>
2024-11-18 20:32:28 +0000

    begin initial solution

--- Chapter 1 - Arrays & Strings/1.2 checkPermutation.js
+++ Chapter 1 - Arrays & Strings/1.2 checkPermutation.js
@@ -5,10 +5,28 @@
 /*
 
 /**
- * --- INITIAL SOLUTION ---
- * 
+ * --- SOLUTION ---
+ * Initial solution will be a brute force solution to check if one string is a permutation of another by calculating all permutations
+ * 1. Calculate all permutations of 1st string
+ * 2. Check if 2nd string is in array of permutations of 1st
+ * string 1 is abc
+ * permutations are:
+ * abc
+ * bac
+ * cba
+ * iterate through the string, lock each character in turn, calculate permutations using the rest of the letters in the string
  */
 
+const checkPermutation = (string1, string2, charIndex = 0, permutations) => {
+  let base = false;
+  if (charIndex === string1.length - 1) base = true;
+  if (base === false) {
+    string1.split('').forEach((char, index) => {})
+  } else {
+
+  }
+
+};
 
 
 /* --- TEST CASES --- */
