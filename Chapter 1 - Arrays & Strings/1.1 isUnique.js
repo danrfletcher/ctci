95e108afcea32fd974c2360ec491613cfd21a233
danrfletcher <danrfletcher@outlook.com>
2024-11-18 18:49:35 +0000

    Addresses:
    You didn't address the follow-up question about not using additional data structures
    The value structure in your Map is more complex than needed - you only need to track existence, not count

--- Chapter 1 - Arrays & Strings/1.1 isUnique.js
+++ Chapter 1 - Arrays & Strings/1.1 isUnique.js
@@ -9,15 +9,25 @@
  * Iterate through string, create bucket for each new character, add 1 to the count each time it is encountered.
  */
 
-const characters = Map();
-
 const isUnique = (string) => {
-    string.forEach(char => characters.has(char) ? false : characters.set(char.toString(), {count: 1}));
-	return true;
+    const characters = Map();
+    return string.forEach(char => characters.has(char) ? false : characters.set(char, true)) || true;
 }
 
 /**
  * --- EXPLANATION ---
- * The above solution has runtime O(N), which is the best possible outcome given the requirement that the algorithm check the string for duplicate characters.
+ * The above solution has time complexity O(N), which is the best possible outcome given the requirement that the algorithm check the string for duplicate characters.
+ * Above solution also has space complexity O(N) as it uses 1 additional data structure to store characters in the string.
  */
 
+/**
+ * --- EXTENDED SOLUTION ---
+ * Without using additional data structures.
+ */
+
+const isUniqueNoExtraData = (string) => {
+    const characters = Map();
+    return string.forEach(charPointer1 => string.forEach((charPointer2) => {
+        if (charPointer1 === charPointer2) return true;
+    })) || true;
+}
