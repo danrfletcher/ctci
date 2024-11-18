1bcbdf743ab51fcf75ef896960d2e930cc38df97
danrfletcher <danrfletcher@outlook.com>
2024-11-18 19:03:30 +0000

    Add checkPermutation problem

---
+++ Chapter 1 - Arrays & Strings/1.2 checkPermutation.js
@@ -0,0 +1,19 @@
+/**
+ * --- PROBLEM DESCRIPTION ---
+ * 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other. 
+ */
+/*
+
+/**
+ * --- INITIAL SOLUTION ---
+ * 
+ */
+
+
+
+/* --- TEST CASES --- */
+
+
+/**
+ * --- EXPLANATION ---
+ */
